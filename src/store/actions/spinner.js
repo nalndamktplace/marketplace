3ae8a9ff0beb3be48abd96588fcd559482822193
z3t4z00k <zetazook@gmail.com>
2022-04-01 16:34:48 +0530

    Created redux store for Wallet, Spinner and Snackbars

---
+++ src/store/actions/spinner.js
@@ -0,0 +1,5 @@
+export const SHOW_SPINNER = "SHOW_SPINNER"
+export const HIDE_SPINNER = "HIDE_SPINNER"
+
+export const showSpinner = data => {return {type: SHOW_SPINNER}}
+export const hideSpinner = data => {return {type: HIDE_SPINNER}}
