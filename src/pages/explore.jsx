ff1e8fa68a120464a02463ab48ee504830b3b2ab
z3t4z00k <zetazook@gmail.com>
2022-04-02 16:31:46 +0530

    Created explore page

---
+++ src/pages/explore.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Page from '../components/hoc/Page/Page'
+
+const ExplorePage = props => {
+	return (
+		<Page>
+			<h3 className='typo__head typo__head--3'>Explore Collections</h3>
+			
+		</Page>
+	)
+}
+
+export default ExplorePage
