ee88174ccfa99de154d35620ee00cd075a244384
z3t4z00k <zetazook@gmail.com>
2022-04-09 17:37:51 +0530

    removed source files

--- src/helpers/functions.js
+++
@@ -1,3 +0,0 @@
-export const isUsable = param => param !== undefined && param !== null
-export const isNull = param => param !== undefined && param === null
-export const isUndefined = param => param === undefined
--- src/store/actions/spinner.js
+++
@@ -1,5 +0,0 @@
-export const SHOW_SPINNER = "SHOW_SPINNER"
-export const HIDE_SPINNER = "HIDE_SPINNER"
-
-export const showSpinner = data => {return {type: SHOW_SPINNER}}
-export const hideSpinner = data => {return {type: HIDE_SPINNER}}
