55cf30867309d82e0cdbf43a398771c3b8e6cb6d
z3t4z00k <zetazook@gmail.com>
2022-04-09 15:38:55 +0530

    updated content, added ability to purchase NFTs

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux'
 import Page from '../components/hoc/Page/Page'
 import Contracts from '../connections/contracts'
 import { isUsable } from '../helpers/functions'
+import { setSnackbar } from '../store/actions/snackbar'
 import { hideSpinner, showSpinner } from '../store/actions/spinner'
 
 const ExplorePage = props => {
@@ -18,7 +19,9 @@ const ExplorePage = props => {
 		else dispatch(hideSpinner())
 	}, [Loading, dispatch])
 
-	useEffect(() => {
+	useEffect(() => { loadNftHandler() }, [])
+
+	const loadNftHandler = () => {
 		setLoading(true)
 		Contracts.loadNfts().then(res => {
 			setLoading(false)
@@ -27,19 +30,37 @@ const ExplorePage = props => {
 			setLoading(false)
 			console.log({err})
 		})
-	}, [])
+	}
+
+	const buyHandler = nft => {
+		setLoading(true)
+		Contracts.buyNft(nft).then(res => {
+			setLoading(false)
+			dispatch(setSnackbar({show: true, message: "Book purchased.", type: 1}))
+			loadNftHandler()
+		}).catch(err => {
+			setLoading(false)
+			if(err.code === 4001)
+				dispatch(setSnackbar({show: true, message: "Transaction denied by user.", type: 3}))
+			console.log({err})
+		})
+	}
 
 	const renderNfts = () => {
 		if(isUsable(Nfts) && Nfts.length>0){
 			let nftDOM = []
 			Nfts.forEach(nft => {
 				nftDOM.push(
-					<div>
-						<img src={nft.image} alt={nft.name} />
-						<p>{nft.name}</p>
-						<p>{nft.description}</p>
-						<p>{nft.price}&nbsp;ETH</p>
-						<div onClick={()=>{Contracts.buyNft(nft)}}>Buy</div>
+					<div className='explore__data__books__item'>
+						<img className='explore__data__books__item__cover' src={nft.image} alt={nft.name} />
+						<div className="explore__data__books__item__data">
+							<p className='explore__data__books__item__data__author typo__body typo__body--2'>{nft.description}</p>
+							<p className='explore__data__books__item__data__name typo__body typo__body--2'>{nft.name}</p>
+						</div>
+						<div className="explore__data__books__item__action">
+							<div onClick={()=>buyHandler(nft)}>Buy</div>
+							<p className='explore__data__books__item__action__price typo__body typo__body--2'>{nft.price}&nbsp;ETH</p>
+						</div>
 					</div>
 				)
 			})
@@ -49,9 +70,17 @@ const ExplorePage = props => {
 	}
 
 	return (
-		<Page>
-			<h3 className='typo__head typo__head--3'>Explore Collections</h3>
-			{renderNfts()}
+		<Page noFooter={true} fluid={true} containerClass={'explore'}>
+			<div className="explore__head">
+				<h3 className='typo__head typo__head--3'>Explore Collection</h3>
+			</div>
+			<div className="explore__data">
+				<div className="explore__data__books">
+					<div className="explore__data__books__wrapper">
+						{renderNfts()}
+					</div>
+				</div>
+			</div>
 		</Page>
 	)
 }
