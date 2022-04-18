8fbb3620a51c8b0da9195002dcf32ac37b6dedb1
z3t4z00k <zetazook@gmail.com>
2022-04-18 18:22:17 +0530

    Created another fn

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,3 +1,4 @@
+export const isFilled = param => param !== undefined && param !== null && param.length>0
 export const isUsable = param => param !== undefined && param !== null
 export const isNull = param => param !== undefined && param === null
 export const isUndefined = param => param === undefined
