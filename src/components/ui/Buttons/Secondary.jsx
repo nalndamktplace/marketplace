1e6a191ae4595b043b9cf2d5989356aec3f344d5
z3t4z00k <zetazook@gmail.com>
2022-04-25 13:35:32 +0530

    Added variants

--- src/components/ui/Buttons/Secondary.jsx
+++ src/components/ui/Buttons/Secondary.jsx
@@ -8,9 +8,7 @@ const SecondaryButton = props => {
 
 	const getClasses = () => {
 		let classes = ["button button--secondary"]
-		if(props.type === 2){
-			classes.push("button--secondary--2")
-		}
+		if(props.theme === 2) classes.push("button--secondary--2")
 		else if (props.type === 3) classes.push("button--secondary--3")
 		return classes.join(' ')
 	}
