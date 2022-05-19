68b9aca7903d48b63d6b2c34fb40b6fcdc25c247
z3t4z00k <zetazook@gmail.com>
2022-05-19 19:06:38 +0530

    Removed extra import statement

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import { useNavigate } from 'react-router'
 
 import Page from '../components/hoc/Page/Page'
