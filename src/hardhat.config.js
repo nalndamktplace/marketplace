797a59391c85c4cfc00e2699e1d26cba8d76acbf
z3t4z00k <zetazook@gmail.com>
2022-05-19 13:00:35 +0530

    Added configs for different networks

--- src/hardhat.config.js
+++ src/hardhat.config.js
@@ -1,14 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-const projectId = process.env.REACT_APP_NET_PROJECT_ID
+const WALLET_KEY = process.env.PRIVATE_KEY
+const ALCHEMY_ID = process.env.ALCHEMY_PROJECT_ID
+const INFURA_ID = process.env.INFURA_PROJECT_ID
 
 module.exports = {
 	networks: {
 		hardhat: {
 			chainId: 1337
 		},
-		mumbai: {
-			url: "https://polygon-mumbai.infura.io/v3/"+projectId
+		mumbaiInfura: {
+			url: `https://polygon-mumbai.infura.io/v3/${INFURA_ID}`
+		},
+		mumbaiAlchemy: {
+			url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_ID}`,
+			accounts: [WALLET_KEY]
 		}
 	},
 	solidity: "0.8.13",
