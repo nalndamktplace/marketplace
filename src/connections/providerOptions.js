f04bf32c6261b1f18bdbfffd2817ace75c13a21e
z3t4z00k <zetazook@gmail.com>
2022-05-26 17:33:06 +0530

    Only sequence and meta mask wallet will work

--- src/connections/providerOptions.js
+++ src/connections/providerOptions.js
@@ -1,13 +1,13 @@
 import { sequence } from "0xsequence"
 // import WalletConnectProvider from "@walletconnect/web3-provider"
-import CoinbaseWalletSDK from "@coinbase/wallet-sdk"
+// import CoinbaseWalletSDK from "@coinbase/wallet-sdk"
 
 const providerOptions = {
 	sequence: {
 		package: sequence,
 		options: {
 			appName: "Nalnda",
-			defaultNetwork: "polygon",
+			defaultNetwork: "mumbai",
 		},
 	},
 	// walletconnect: {
@@ -16,15 +16,15 @@ const providerOptions = {
 	//		 infuraId: "INFURA_ID",
 	//	 },
 	// },
-	coinbasewallet: {
-		package: CoinbaseWalletSDK,
-		options: {
-			appName: "Nalnda",
-			rpc: process.env.REACT_APP_CONTRACTS_URI,
-			chainId: 1337,
-			darkMode: false,
-		},
-	},
+	// coinbasewallet: {
+	// 	package: CoinbaseWalletSDK,
+	// 	options: {
+	// 		appName: "Nalnda",
+	// 		rpc: process.env.REACT_APP_CONTRACTS_URI,
+	// 		chainId: 1337,
+	// 		darkMode: false,
+	// 	},
+	// },
 }
 
 export default providerOptions
