fba4c7157e28448d4cd8bb263412185194cdb929
goodBits <ak.onhtrack@gmail.com>
2022-05-29 11:12:52 +0530

    Integrated Bookmarks API

--- src/components/ui/BookmarkPanel/BookmarkPanel.jsx
+++ src/components/ui/BookmarkPanel/BookmarkPanel.jsx
@@ -1,18 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { isUsable } from "../../../helpers/functions";
 import {ReactComponent as TrashIcon} from "../../../assets/icons/trash-icon.svg";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { setSnackbar } from "../../../store/actions/snackbar";
+import { BASE_URL } from "../../../config/env";
+import Contracts from "../../../connections/contracts";
 
 const BookMarkPanel = ({rendition,bookMeta,onAdd=()=>{},onRemove=()=>{},onGoto=()=>{}}) => {
 
     const [bookmarkTitle, setBookmarkTitle] = useState("");
+    const [bookmarks, setBookmarks] = useState([]);
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
+    const [Wallet, setWallet] = useState(null);
+
+    useEffect(() => {
+		if(isUsable(bookMeta) && isUsable(Wallet)){
+			setLoading(true)
+			axios({
+				url: BASE_URL+'/api/reader/bookmarks?bid='+bookMeta.book_address+'&uid='+Wallet,
+				method: 'GET'
+			}).then(res => {
+				if(res.status === 200) {
+                    let parsedBookmarks = JSON.parse(res.data.bookmarks)||[] ;
+                    setBookmarks(parsedBookmarks);
+                    const bookKey = `${bookMeta.id}:bookmarks`
+                    localStorage.setItem(bookKey,JSON.stringify(parsedBookmarks));
+                }
+				else dispatch(setSnackbar('NOT200'))
+			}).catch(err => {
+				dispatch(setSnackbar('ERROR'))
+			}).finally(() => setLoading(false))
+		}
+	}, [bookMeta, Wallet, dispatch])
+
+    useEffect(() => {
+		Contracts.Wallet.getWalletAddress().then(res => {
+			if(isUsable(res)) setWallet(res)
+		}).catch(err =>{
+			console.error(err);
+		})
+	}, [])
 
     const renderBookmarkedItems = () => {
         let domItems = [] ;
         if(!isUsable(rendition)) return "";
         if(!isUsable(bookMeta)) return "";
-        const bookKey = `${bookMeta.id}:bookmarks`
-        let stored = JSON.parse(window.localStorage.getItem(bookKey)) || [];
-        stored.forEach((item,i)=>{
+        bookmarks.forEach((item,i)=>{
             domItems.push(
                 <div key={i} className="bookmark-panel__container__item" onClick={()=>gotoBookmarkedPage(`${item.cfi}`)}>
                     <div className="bookmark-panel__container__item__name">{item.title}</div>
@@ -33,26 +68,54 @@ const BookMarkPanel = ({rendition,bookMeta,onAdd=()=>{},onRemove=()=>{},onGoto=(
     const addBookMark = () => {
         if(!isUsable(rendition)) return;
         if(!isUsable(bookMeta)) return;
-        const bookKey = `${bookMeta.id}:bookmarks`
-        let stored = JSON.parse(window.localStorage.getItem(bookKey)) || [];
-        window.localStorage.setItem(bookKey,JSON.stringify([...stored,{
+        if(!bookmarkTitle) return;
+        let newBookmarks = [...bookmarks,{
             title : bookmarkTitle || "Untitled",
             cfi : rendition.currentLocation().start.cfi,
             percent : rendition.currentLocation().start.percentage
-        }]))
-        setBookmarkTitle("");
-        onAdd();
+        }];
+        axios({
+            url: BASE_URL+'/api/reader/bookmarks?bid='+bookMeta.book_address+'&uid='+Wallet,
+            method: 'POST',
+            data: {
+                bookmarks: JSON.stringify(newBookmarks),
+            }
+        }).then(res => {
+            if(res.status === 200) {
+                setBookmarkTitle("");
+                setBookmarks(newBookmarks);
+                const bookKey = `${bookMeta.id}:bookmarks`
+                localStorage.setItem(bookKey,JSON.stringify(newBookmarks));
+                onAdd();
+            } 
+            else dispatch(setSnackbar('NOT200'))
+        }).catch(err => {
+            dispatch(setSnackbar('ERROR'))
+        }).finally(() => setLoading(false))
     }
 
     const removeBookMark = (itemIndex) => {
         if(!isUsable(rendition)) return;
         if(!isUsable(bookMeta)) return;
-        const bookKey = `${bookMeta.id}:bookmarks`
-        let stored = JSON.parse(window.localStorage.getItem(bookKey)) || [];
-        stored = stored.filter((item,i) => i != itemIndex );
-        window.localStorage.setItem(bookKey,JSON.stringify(stored))
-        setBookmarkTitle("");
-        onRemove();
+        let newBookmarks = bookmarks.filter((item,i) => i != itemIndex );
+        axios({
+            url: BASE_URL+'/api/reader/bookmarks?bid='+bookMeta.book_address+'&uid='+Wallet,
+            method: 'POST',
+            data: {
+                bookmarks: JSON.stringify(newBookmarks),
+            }
+        }).then(res => {
+            if(res.status === 200) {
+                setBookmarkTitle("");
+                setBookmarks(newBookmarks);
+                const bookKey = `${bookMeta.id}:bookmarks`
+                localStorage.setItem(bookKey,JSON.stringify(newBookmarks));
+                onRemove();
+            } 
+            else dispatch(setSnackbar('NOT200'))
+        }).catch(err => {
+            dispatch(setSnackbar('ERROR'))
+        }).finally(() => setLoading(false))
     }
 
     const gotoBookmarkedPage = (cfi) => {
@@ -80,10 +143,11 @@ const BookMarkPanel = ({rendition,bookMeta,onAdd=()=>{},onRemove=()=>{},onGoto=(
                 </button>
             </div>
             <div className="bookmark-panel__title">
-                Bookmarked Pages
+                Bookmarked Pages 
             </div>
             <div className="bookmark-panel__container">
                 {renderBookmarkedItems()}
+                {loading && "Loading..."}
             </div>
         </div>
     );
