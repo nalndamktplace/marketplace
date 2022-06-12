9ce7f991c1b8fad39fa98b7321e1ce8687378fcd
z3t4z00k <zetazook@gmail.com>
2022-06-12 19:44:23 +0530

    Updated contracts, genres and languages

--- src/config/languages.js
+++ src/config/languages.js
@@ -1,5 +1,17 @@
-export const LANGUAGES = {
-    'ES - Espanol':1, 
-    'EN - English':2, 
-    'HN - Hindi'  :3
-}
+export const LANGUAGES = [
+	"EN - English",
+	"ZH - Chinese",
+	"DE - German",
+	"ES - Spanish",
+	"JA - Japanese",
+	"RU - Russian",
+	"FR - French",
+	"KO - Korean",
+	"IT - Italian",
+	"NL - Dutch",
+	"PT - Portugese",
+	"HI - Hindi",
+	"PL - Polish",
+	"DA - Danish",
+	"BN - Bangla"
+]
