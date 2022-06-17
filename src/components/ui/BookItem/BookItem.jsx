cb37cb44997f2abba607d93d9cfa441d45979cc3
goodBits <ak.onhtrack@gmail.com>
2022-06-17 23:44:24 +0530

    Changed USDC logo, moved logo in front of prices

--- src/components/ui/BookItem/BookItem.jsx
+++ src/components/ui/BookItem/BookItem.jsx
@@ -7,7 +7,7 @@ const BookItem = ({book,onBuy=()=>{},onOpen=()=>{},layout="GRID"}) => {
         <div className='book-item' onClick={onOpen} data-layout={layout}>
             <img className='book-item__cover' src={book.cover} alt={book.name} />
             <div className="book-item__data">
-                <div className='book-item__data__price typo__body typo__body--2 utils__d__flex utils__align__center'>{book.price}&nbsp;<USDCIcon width={20} height={20} fill="currentColor"/></div>
+                <div className='book-item__data__price typo__body typo__body--2 utils__d__flex utils__align__center'><USDCIcon width={20} height={20} stroke="currentColor"/>&nbsp;{book.price}</div>
                 <div className='book-item__data__name typo__body typo__body--2'>{book.title}</div>
                 <div className='book-item__data__author typo__body typo__body--2'>{book.author}</div>
                 <div className='book-item__data__synopsis typo__body typo__body--2 typo__color--n500'>{book.synopsis.split(" ").slice(0,40).join(" ")}...</div>
