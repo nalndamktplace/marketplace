e8c3a421e35fcc29f58ad7f7cb8458a582e71e3f
goodBits <ak.onhtrack@gmail.com>
2022-06-24 21:01:49 +0530

    Fixed reader

--- src/pages/Reader.jsx
+++ src/pages/Reader.jsx
@@ -33,6 +33,7 @@ import RangeSlider from "../components/ui/RangeSlider/RangeSlider"
 import { setWallet } from "../store/actions/wallet"
 import Wallet from "../connections/wallet"
 import axios from "axios"
+import readerTheme from "../config/readerTheme"
 
 const ReaderPage = () => {
 
@@ -53,10 +54,17 @@ const ReaderPage = () => {
 	const [customizerPanel, setCustomizerPanel] = useState(false)
 	const [tocPanel, setTocPanel] = useState(false);
 	const [WalletAddress, setWalletAddress] = useState(null);
+	const [showUI, setShowUI] = useState(true);
+	const [annotationSelection, setAnnotationSelection] = useState({})
+	const [showContextMenu, setShowContextMenu] = useState(false)
+	const [chapterName, setChapterName] = useState("");
+	const [currentLocationCFI, setCurrentLocationCFI] = useState("");
 
 	const debouncedProgress = useDebounce(progress, 300)
+	const addAnnotationRef = useRef()
 	const seeking = useRef(false);
 
+	// todo confirm what to do for preview
 	const connectWallet = useCallback(
 		() => {
 			Wallet.connectWallet().then(res => {
@@ -69,207 +77,14 @@ const ReaderPage = () => {
 		},[dispatch],
 	)
 
-	useEffect(() => {
-		setLoading(true)
-		if(isUsable(WalletState))
-			setWalletAddress(WalletState)
-		else connectWallet()
-		setLoading(false)
-	}, [WalletState, connectWallet])
-
-	useEffect(() => { GaTracker('page_view_reader') }, [])
-
-	useEffect(() => {
-		if(Loading) dispatch(showSpinner())
-		else dispatch(hideSpinner())
-	}, [Loading, dispatch])
-
-	useEffect(() => {
-		if(!isUsable(Math.floor(progress*100/totalLocations)) || isNaN(Math.floor(progress*100/totalLocations))) setLoading(true)
-		else setLoading(false)
-	}, [progress, totalLocations])
-
-	const hideAllPanel = ({customizer=true,annotation=true,toc=true}) => {
-		customizer && setCustomizerPanel(false)
-		annotation && setAnnotaionPanel(false)
-		toc && setTocPanel(false)
-	}
-
-	useEffect(()=>{
-		if(!isUsable(rendition)) return
-		const handleResize = () => {
-			GaTracker('event_reader_resize')
-			rendition.manager.resize(window.innerWidth<600 ? window.innerWidth : window.innerWidth-128,"100%")
-		}
-		const handleFullscreen = () => {
-			if(isUsable(window.document.fullscreenElement)){
-				GaTracker('event_reader_fullscreen')
-				setFullscreen(true)
-			}
-			else{
-				GaTracker('event_reader_window')
-				setFullscreen(false)
-			}
-			handleResize()
-		}
-		window.addEventListener("resize",handleResize)
-		window.addEventListener("fullscreenchange",handleFullscreen)
-		return () => {
-			window.removeEventListener("resize",handleResize)
-			window.removeEventListener("fullscreenchange",handleFullscreen)
-		}
-	},[rendition])
-
-	useEffect(()=>{
-		let bookURL = null
-		const navParams = params.state
-		if(isUsable(navParams.preview) && navParams.preview === true){
-			bookURL = BASE_URL+'/files/'+navParams.book.preview
-			setBookMeta(navParams.book)
-			setPreview(true)
-		}
-		else{
-			bookURL = navParams.book.submarineURL
-			setBookMeta(navParams.book)
-			setPreview(false)
-		}
-		const book = Epub(bookURL,{openAs:"epub"})
-		book.ready.then(()=>{
-			document.querySelector("#book__reader").innerHTML = ""
-			const rendition = book.renderTo("book__reader", { 
-				width: window.innerWidth<600 ? window.innerWidth : window.innerWidth-128,
-				height: "100%" ,
-				manager: "continuous",
-				flow: "paginated",
-				snap : "true"
-			})
-			rendition.themes.registerThemes({
-				dark : {
-					body : {
-						"background-color" : "black",
-						"color" : "white",
-						"--font-family" : "inherit",
-						"--line-height" : "1.6"
-					},
-					p : {
-						"text-align": "justify" ,
-						"font-family" : "var(--font-family) !important",
-						"line-height" : "var(--line-height) !important"
-					}
-				},
-				light : {
-					body : {
-						"background-color" : "white",
-						"color" : "black",
-						"--font-family" : "inherit",
-						"--line-height" : "1.6"
-					},
-					p : {
-						"text-align": "justify" ,
-						"font-family" : "var(--font-family) !important",
-						"line-height" : "var(--line-height) !important"
-					}
-				}
-			})
-			rendition.themes.select("light")
-			rendition.display()
-			rendition.themes.fontSize("150%");
-			rendition.themes.font("Arial,sans-serif");
-			setRendition(rendition)
-		}).catch(err => {
-			console.error({err})
-			dispatch(setSnackbar({show: true, message: "Error while loading book.", type: 4}))
-		})
-	},[params, dispatch])
-
 	const saveLastReadPage = useCallback(
 		(cfi) => {
 			if(!isUsable(window.localStorage)) return
 			if(!isUsable(bookMeta)) return
 			const bookKey = `${bookMeta.id}:lastread`
 			localStorage.setItem(bookKey,cfi)
-		},
-		[bookMeta],
+		},[bookMeta]
 	)
-
-	useEffect(()=>{
-		if (!isUsable(rendition)) return
-		if (!isUsable(bookMeta)) return
-		rendition.on("relocated", (event)=>{
-			setProgress(event.start.location)
-			saveLastReadPage(event.start.cfi)
-		})
-	},[bookMeta,rendition, saveLastReadPage])
-
-	useEffect(()=>{
-		if (!isUsable(rendition)) return
-		if (!isUsable(bookMeta)) return
-		const bookKey = `${bookMeta.id}:locations`
-		let stored = localStorage.getItem(bookKey)
-		if (stored) {
-			rendition.book.locations.load(stored)
-			setTotalLocations(JSON.parse(stored).length)
-		} else {
-			rendition.book.locations.generate().then(()=>{
-				setTotalLocations(rendition.book.locations.total)
-				localStorage.setItem(bookKey, rendition.book.locations.save())
-			}).catch((err)=>{
-				console.error(err)
-			})
-		}
-	},[rendition,bookMeta])
-
-	useEffect(() => {
-		if(!isUsable(rendition)) return
-		if(seeking.current==true){
-			rendition.display(rendition.book.locations.cfiFromLocation(debouncedProgress))
-			seeking.current=false
-		}	
-	}, [debouncedProgress, rendition])
-
-	const handlePageUpdate = (e) => {
-		seeking.current = true ;
-		setProgress(e.target.value)
-	}
-
-	// todo move loading to server
-	useEffect(() => {
-		if(!isUsable(window.localStorage)) return
-		if(!isUsable(bookMeta)) return
-		if(!isUsable(rendition)) return
-		const bookKey = `${bookMeta.id}:lastread`
-		let lastPageCfi = localStorage.getItem(bookKey)
-		if(isUsable(lastPageCfi)) {
-			rendition.display(lastPageCfi)
-		}
-	}, [bookMeta, rendition]) 
-	
-	function openFullscreen() {
-		var elem = document.documentElement
-		if (elem.requestFullscreen) {
-			elem.requestFullscreen()
-		} else if (elem.webkitRequestFullscreen) { /* Safari */
-			elem.webkitRequestFullscreen()
-		} else if (elem.msRequestFullscreen) { /* IE11 */
-			elem.msRequestFullscreen()
-		}
-	}
-
-	function closeFullscreen() {
-		if(!document.fullscreenElement) return
-		if (document.exitFullscreen) {
-			document.exitFullscreen()
-		} else if (document.webkitExitFullscreen) { /* Safari */
-			document.webkitExitFullscreen()
-		} else if (document.msExitFullscreen) { /* IE11 */
-			document.msExitFullscreen()
-		}
-	}
-
-	useEffect(()=>{
-		if(fullscreen===true) openFullscreen()
-		else closeFullscreen()
-	},[fullscreen])
 
 	const isCurrentPageBookmarked = useCallback(
 		() => {
@@ -300,57 +115,191 @@ const ReaderPage = () => {
 		},[isCurrentPageBookmarked]
 	)
 
-	const [annotationSelection, setAnnotationSelection] = useState({})
-	const [showContextMenu, setShowContextMenu] = useState(false)
-	// const [contextMenuPosition, setContextMenuPosition] = useState({x:0,y:0})
-	// const contextMenuContainerRef = useRef()
+	const hideAllPanel = useCallback(
+		({customizer=true,annotation=true,toc=true}={}) => {
+			customizer && setCustomizerPanel(false)
+			annotation && setAnnotaionPanel(false)
+			toc && setTocPanel(false)
+		},
+		[]
+	)
 
-	// useEffect(()=>{
-	//	 if(!isUsable(contextMenuContainerRef.current)) return
-	//	 contextMenuContainerRef.current.style.setProperty("--x",contextMenuPosition.x)
-	//	 contextMenuContainerRef.current.style.setProperty("--y",contextMenuPosition.y)
-	// },[contextMenuContainerRef,contextMenuPosition])
+	useEffect(() => { GaTracker('page_view_reader') }, [])
+
+	useEffect(()=>{
+		if(fullscreen===true) openFullscreen()
+		else closeFullscreen()
+	},[fullscreen])	
+
+	useEffect(() => {
+		if(Loading) dispatch(showSpinner())
+		else dispatch(hideSpinner())
+	}, [Loading, dispatch])
+
+	useEffect(() => {
+		setLoading(true)
+		if(isUsable(WalletState)) setWalletAddress(WalletState)
+		else connectWallet()
+		setLoading(false)
+	}, [WalletState, connectWallet])
+
+	useEffect(()=>{
+		hideAllPanel()
+	},[showUI,hideAllPanel])
+
+	useEffect(()=>{
+		let bookURL = null
+		const navParams = params.state
+		if(isUsable(navParams.preview) && navParams.preview === true){
+			bookURL = BASE_URL+'/files/'+navParams.book.preview
+			setPreview(true)
+		} else {
+			bookURL = navParams.book.submarineURL
+			setPreview(false)
+		}
+
+		const book = Epub(bookURL,{openAs:"epub"})
+		book.ready.then(()=>{
+			const elm = document.querySelector("#book__reader") ;
+			if(elm) elm.innerHTML = "";
+			const _rendition = book.renderTo("book__reader", {
+                width: "100%",
+                height: "100%",
+                manager: "continuous",
+                flow: "paginated",
+                snap: "true",
+            });
+			_rendition.display().then(()=>{
+				_rendition.themes.registerThemes(readerTheme)
+				_rendition.themes.select("light")
+				_rendition.themes.fontSize("170%")
+				_rendition.themes.override("--font-family", "Arial,sans-serif")
+			});
+			setRendition(_rendition)
+		}).catch(err => {
+			console.error({err})
+			dispatch(setSnackbar({show: true, message: "Error while loading book.", type: 4}))
+		})
+		setBookMeta(navParams.book)  
+	},[params, dispatch])
+
+	useEffect(()=>{ 
+		if(!isUsable(rendition)) return
+		const handleResize = () => {
+			GaTracker('event_reader_resize')
+			rendition.manager.resize("100%","100%")
+		}
+		const handleFullscreen = () => {
+			if(isUsable(window.document.fullscreenElement)){
+				GaTracker('event_reader_fullscreen')
+				setFullscreen(true)
+			}
+			else{
+				GaTracker('event_reader_window')
+				setFullscreen(false)
+			}
+			handleResize()
+		}
+		window.addEventListener("resize",handleResize)
+		window.addEventListener("fullscreenchange",handleFullscreen)
+		return () => {
+			window.removeEventListener("resize",handleResize)
+			window.removeEventListener("fullscreenchange",handleFullscreen)
+		}
+	},[rendition])
+
+	useEffect(()=>{
+		if(!isUsable(rendition)) return
+		if(!isUsable(currentLocationCFI) && !isFilled(currentLocationCFI)) return
+		rendition.book.loaded.navigation.then(function(){
+			let locationCfi = currentLocationCFI;
+			let spineItem = rendition.book.spine.get(locationCfi);
+			if(!isUsable(spineItem)) return ;
+			let navItem = rendition.book.navigation.get(spineItem.href);
+			setChapterName(navItem?.label?.trim()||"")
+		});
+	},[rendition,currentLocationCFI])
 
 	useEffect(()=>{
 		if(!isUsable(rendition)) return
 		if(!isUsable(bookMeta)) return
-		const handleRelocated = ()=>{updateBookmarkedStatus()}
+
+		const handleRelocated = (event)=>{
+			updateBookmarkedStatus()
+			setProgress(event.start.location)
+			saveLastReadPage(event.start.cfi)
+			setCurrentLocationCFI(event.start.cfi)
+		}
+
+		const handleClick = () => { setShowUI(s=>!s)}
+		const handleKeyUp = (e) => {
+			if ((e.key === "ArrowLeft") || (e.keyCode || e.which) === 37) {
+				rendition.prev();
+			}
+			if ((e.key === "ArrowRight") || (e.keyCode || e.which) === 39) {
+				rendition.next();
+			}
+		}
+
 		rendition.on("relocated",handleRelocated)
-		return ()=>{rendition.off("relocated",handleRelocated)}
-	},[rendition,bookMeta, updateBookmarkedStatus])
+		rendition.on("click",handleClick);
+		rendition.on("keyup", handleKeyUp);
+		document.addEventListener("keyup", handleKeyUp);
+		return ()=>{
+			rendition.off("relocated",handleRelocated)
+			rendition.off("click",handleClick);
+			rendition.off("keyup", handleKeyUp);
+			document.removeEventListener("keyup", handleKeyUp);
+		}
+	},[rendition,bookMeta, updateBookmarkedStatus,saveLastReadPage,setCurrentLocationCFI])
+
+	// Generates Locations for book
+	useEffect(()=>{
+		if (!isUsable(rendition)) return
+		if (!isUsable(bookMeta)) return
+		const bookKey = `${bookMeta.id}:locations`
+		let stored = localStorage.getItem(bookKey)
+		if (stored) {
+			rendition.book.locations.load(stored)
+			setTotalLocations(JSON.parse(stored).length)
+		} else {
+			rendition.book.locations.generate()
+			.then(()=>{
+				setTotalLocations(rendition.book.locations.total)
+				localStorage.setItem(bookKey, rendition.book.locations.save())
+			})
+			.catch((err)=>{console.error(err)})
+		}
+	},[rendition,bookMeta])
+
+	useEffect(() => {
+		if(!isUsable(rendition)) return
+		if(seeking.current === true){
+			rendition.display(rendition.book.locations.cfiFromLocation(debouncedProgress))
+			seeking.current=false
+		}	
+	}, [debouncedProgress, rendition, seeking])
+
+	// todo move loading to server
+	useEffect(() => {
+		if(!isUsable(window.localStorage)) return
+		if(!isUsable(bookMeta)) return
+		if(!isUsable(rendition)) return
+		const bookKey = `${bookMeta.id}:lastread`
+		let lastPageCfi = localStorage.getItem(bookKey)
+		if(isUsable(lastPageCfi)) {
+			rendition.display(lastPageCfi)
+		}
+	}, [bookMeta, rendition]) 
+
+
 
 	useEffect(()=>{
 		if(!isUsable(rendition)) return
 		if(!isUsable(bookMeta)) return
 		const handleSelected = (cfiRange,contents)=>{
-			// console.log(cfiRange,contents)
-			// const selection = contents.window.getSelection()
-			// const anchorNodeCoords = selection.anchorNode.parentElement.getBoundingClientRect()
-			// const extentNodeCoords = selection.extentNode.parentElement.getBoundingClientRect()
-
-			// let coords = {
-			//	 y : (Math.min(anchorNodeCoords.y,extentNodeCoords.y) + Math.max(anchorNodeCoords.y+anchorNodeCoords.height,extentNodeCoords.y+extentNodeCoords.height))/2,
-			//	 x : (Math.min(anchorNodeCoords.x,extentNodeCoords.x) + Math.max(anchorNodeCoords.x+anchorNodeCoords.width,extentNodeCoords.x+extentNodeCoords.width))/2,
-			// }
-
-			// coords = {
-			//	 x : window.innerWidth / 2,
-			//	 y : window.innerHeight / 2,
-			// }
-
-			// if(isNaN(coords.x) || isNaN(coords.y)){
-			//	 coords = {
-			//		 x : window.innerWidth / 2,
-			//		 y : window.innerHeight / 2,
-			//	 }
-			// }
-
 			rendition.book.getRange(cfiRange).then((range)=>{
-				setAnnotationSelection({
-					cfiRange,
-					text : range?.toString()
-				})
-				// setContextMenuPosition(coords)
+				setAnnotationSelection({cfiRange,text : range?.toString()})
 				setShowContextMenu(true)
 			}).catch(()=>{
 				setAnnotationSelection({})
@@ -376,7 +325,24 @@ const ReaderPage = () => {
 		updateAnnotation()
 	},[rendition,bookMeta])
 
-	const addAnnotationRef = useRef()
+	const handlePageUpdate = (e) => {
+		seeking.current = true ;
+		setProgress(e.target.value)
+	}
+
+	const openFullscreen = () => {
+		var elem = document.documentElement
+		if (elem.requestFullscreen) elem.requestFullscreen()
+		else if (elem.webkitRequestFullscreen)elem.webkitRequestFullscreen()
+		else if (elem.msRequestFullscreen) elem.msRequestFullscreen()
+	}
+
+	const closeFullscreen = () => {
+		if(!document.fullscreenElement) return
+		if (document.exitFullscreen) document.exitFullscreen()
+		else if (document.webkitExitFullscreen) document.webkitExitFullscreen()
+		else if (document.msExitFullscreen) document.msExitFullscreen()
+	}
 
 	const handleAnnotationColorSelect = (color) => {
 		if(!isUsable(annotationSelection)) return
@@ -410,23 +376,22 @@ const ReaderPage = () => {
 			if(!isUsable(rendition)) return
 			if(!isUsable(bookMeta)) return
 			setLoading(true)
-			let newBookmarks = {
+			let newBookmark = {
 				cfi : rendition.currentLocation().start.cfi,
 				percent : rendition.currentLocation().start.percentage
 			};
-			console.log(newBookmarks)
 			axios({
 				url: BASE_URL+'/api/reader/bookmarks',
 				method: 'POST',
 				data: {
 					bid: bookMeta.id,
 					uid: WalletAddress,
-					bookmarks: JSON.stringify(newBookmarks),
+					bookmarks: JSON.stringify(newBookmark),
 				}
 			}).then(res => {
 				if(res.status === 200) {
 					const bookKey = `${bookMeta.id}:bookmarks`
-					localStorage.setItem(bookKey,JSON.stringify(newBookmarks))
+					localStorage.setItem(bookKey,JSON.stringify(newBookmark))
 					updateBookmarkedStatus()
 				} 
 				else dispatch(setSnackbar('NOT200'))
@@ -442,7 +407,6 @@ const ReaderPage = () => {
 			if(!isUsable(rendition)) return
 			if(!isUsable(bookMeta)) return
 			setLoading(true)
-			let newBookmarks = "" ;
 			axios({
 				url: BASE_URL+'/api/reader/bookmarks',
 				method: 'POST',
@@ -471,16 +435,18 @@ const ReaderPage = () => {
 
 	return (
 		<div className="reader">
-			<div className="reader__header">
+			<div className={"reader__header" + (showUI?" reader__header--show":"")}>
 				<div className="reader__header__left">
 					<Button type="icon" onClick={()=>{navigate(-1)}}><ChevronLeftIcon/></Button>
-					<ReadTimer preview={Preview} bookMeta={bookMeta}/>
+					<div className="reader__header__left__timer">
+						<ReadTimer preview={Preview} bookMeta={bookMeta}/>
+					</div>
 				</div>
 				<div className="reader__header__center">
-					<div className="reader__header__center__title">{bookMeta.title||"Untitled"}</div>
+					<div className="typo__body--2 typo__color--n700">{bookMeta.title||"Untitled"}</div>
 				</div> 
 				<div className="reader__header__right">
-					<Button type="icon" onClick={()=>setFullscreen(s=>!s)}>
+					<Button className="reader__header__right__hide-on-mobile" type="icon" onClick={()=>setFullscreen(s=>!s)}>
 						{fullscreen?<MinimizeIcon/>:<MaximizeIcon/>}
 					</Button>
 					<Button type="icon" className={tocPanel?"reader__header__right__button--active":""} onClick={()=>{hideAllPanel({toc:false});setTocPanel(s=>!s)}} >
@@ -496,7 +462,7 @@ const ReaderPage = () => {
 					<Button type="icon" className={pageBookmarked?"reader__header__right__button--active":""} onClick={toggleBookMark} ><BookmarkIcon /></Button>
 					<Button type="icon" className={customizerPanel?"reader__header__right__button--active":""} onClick={()=>{hideAllPanel({customizer:false});setCustomizerPanel(s=>!s)}}><LetterCaseIcon /></Button>
 					<SidePanel show={customizerPanel} position="right">
-						<Customizer rendition={rendition}/>
+						<Customizer initialFontSize={100} rendition={rendition}/>
 					</SidePanel>
 				</div>
 			</div>
@@ -529,9 +495,14 @@ const ReaderPage = () => {
 					/>
 				</div>
 			</div>
-			<nav className="reader__nav">
-				<RangeSlider value={progress} onChange={handlePageUpdate} max={totalLocations} className="reader__nav__progress" />
-				<div className="reader__nav__progress-value">{Math.floor(progress*100/totalLocations)}%</div>
+			<nav className={"reader__nav" + (showUI?" reader__nav--show":"")}>
+				<div className="reader__nav__value">
+					<div className="reader__nav__value__chapter-title typo__gray--n600">{chapterName}</div>
+					<div>{Math.floor(progress*100/totalLocations)}%</div>
+				</div>
+				<div className="reader__nav__progress">
+					<RangeSlider value={progress} onChange={handlePageUpdate} max={totalLocations} className="reader__nav__progress" />
+				</div>
 			</nav>
 		</div>
 	)
