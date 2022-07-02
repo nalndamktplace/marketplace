f239eb25cc11f64c853bbefe2bf31123810fc7e2
goodBits <ak.onhtrack@gmail.com>
2022-07-02 12:47:33 +0530

    Added checks for indexof

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -484,9 +484,9 @@ const BookPage = props => {
 				})
 			}).catch(err => {
 				setLoading(false)
-				if(err.message.indexOf("execution reverted: ERC20: transfer amount exceeds balance")>-1)
+				if(err?.message?.indexOf("execution reverted: ERC20: transfer amount exceeds balance")>-1)
 					dispatch(setSnackbar({show: true, message: "You do not have enough NALNDA to purchase this book. Please try again later.", type: 3}))
-				if(err.message.indexOf("execution reverted: NalndaBook: Book unapproved from marketplace!")>-1)
+				if(err?.message?.indexOf("execution reverted: NalndaBook: Book unapproved from marketplace!")>-1)
 					dispatch(setSnackbar({show: true, message: "The book has not been approved for sales yet. Please try again later.", type: 3}))
 				else if(err.code === 4001)
 					dispatch(setSnackbar({show: true, message: "Transaction denied by user.", type: 3}))
