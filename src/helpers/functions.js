16a9e0337f5f4d617244f1ae680dea5deb6b8733
z3t4z00k <zetazook@gmail.com>
2022-07-06 12:04:23 +0530

    Added functions to check user states

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,5 +1,28 @@
+import moment from "moment"
+
 export const isNotEmpty = param => param !== undefined && param !== null && param !== ""
 export const isFilled = param => param !== undefined && param !== null && param.length>0
 export const isUsable = param => param !== undefined && param !== null
 export const isNull = param => param !== undefined && param === null
 export const isUndefined = param => param === undefined
+
+export const isUserLoggedIn = data => {
+	if(isUsable(data)){
+		const user = data.user
+		const tokens = data.tokens
+		if(isUsable(tokens.acsTkn)){
+			if(moment().isBefore(tokens.acsTkn.exp) && isUsable(user.uid)) return true
+		}
+	}
+	return false
+}
+
+export const isWalletConnected = data => {
+	if(isUsable(data)){
+		const wallet = data.wallet
+		if(isUsable(wallet)){
+			if(isUsable(wallet.wallet) && isUsable(wallet.provider) && isUsable(wallet.signer) && isUsable(wallet.address)) return true
+		}
+	}
+	return false
+}
