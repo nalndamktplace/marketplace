756cf092a9c19bbdc4c7879d0cb6ea3c50752cdb
z3t4z00k <zetazook@gmail.com>
2022-07-10 09:17:55 +0530

    Updated wallet debig page

--- src/Debug/Wallet.jsx
+++ src/Debug/Wallet.jsx
@@ -4,10 +4,18 @@ import Wallet from '../connections/wallet'
 
 const WalletDebugPage = props => {
 
+	const [Signer, setSigner] = useState(null)
+	const [Address, setAddress] = useState(null)
 	const [Provider, setProvider] = useState(null)
+	const [WalletInstance, setWalletInstance] = useState(null)
 
-	const walletConnectHanlder = async() => {
-		setProvider(await Wallet.connectWallet())
+	const walletConnectHanlder = () => {
+		Wallet.connectWallet().then(res => {
+			setSigner(res.signer)
+			setAddress(res.address)
+			setProvider(res.provider)
+			setWalletInstance(res.wallet)
+		})
 	}
 
 	const getWalletAddress = () => {
@@ -18,9 +26,9 @@ const WalletDebugPage = props => {
 		<React.Fragment>
 			<Button onClick={()=>walletConnectHanlder()}>Connect Wallet</Button>
 			<Button onClick={()=>Wallet.disconnectWallet()}>Disconnect Wallet</Button>
-			<Button onClick={()=>getWalletAddress()}>Get Wallet Address</Button>
-			<Button onClick={()=>Wallet.getAccounts()}>Get Accounts</Button>
-			<Button onClick={()=>Wallet.getChainID()}>Get ChainID</Button>
+			<Button onClick={()=>getWalletAddress(Signer)}>Get Wallet Address</Button>
+			<Button onClick={()=>Wallet.getAccounts(Provider)}>Get Accounts</Button>
+			<Button onClick={()=>Wallet.getChainID(Signer)}>Get ChainID</Button>
 			<Button onClick={()=>Wallet.getSigner()}>Get Signer</Button>
 			<Button onClick={()=>Wallet.getProvider()}>Get Provider</Button>
 			<Button onClick={()=>Wallet.getBalance()}>Get Balance</Button>
