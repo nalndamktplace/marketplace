03f6c7c008ddd6b0ad161178fa10eac4c9938f58
z3t4z00k <zetazook@gmail.com>
2022-07-10 08:18:06 +0530

    Updated wallet HOC

--- src/components/hoc/Wallet/WalletHOC.jsx
+++ src/components/hoc/Wallet/WalletHOC.jsx
@@ -1,38 +1,41 @@
-import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
 import Wallet from '../../../connections/wallet'
 
-import { setUser } from '../../../store/actions/user'
 import { setWallet } from '../../../store/actions/wallet'
 
-import { BASE_URL } from '../../../config/env'
+import { useSelector } from 'react-redux'
+import { isWalletConnected } from '../../../helpers/functions'
 
 const WalletHOC = props => {
 
 	const dispatch = useDispatch()
 
+	const WalletState = useSelector(state => state.WalletState)
+
 	useEffect(() => {
 		if(Wallet.web3Modal.cachedProvider){
 			Wallet.connectWallet().then(res => {
 				dispatch(setWallet({ wallet: res.wallet, provider: res.provider, signer: res.signer, address: res.address }))
-				axios({
-					url: BASE_URL+'/api/user/login',
-					method: 'POST',
-					headers: {
-						'address': res.address 
-					}
-				}).then(res => {
-					if(res.status === 200) dispatch(setUser(res.data))
-				}).catch(err => {
-				}).finally( () => {
-				})
-			}).catch(err => {
-				// navigate('/')
-			})
+			}).catch(err => { })
 		}
 	}, [dispatch])
+
+	useEffect(() => {
+		if(isWalletConnected(WalletState)){
+			WalletState.wallet.wallet.on("accountsChanged", accounts => console.log({accounts}))
+			WalletState.wallet.wallet.on("chainChanged", chainId => console.log({chainId}))
+			WalletState.wallet.wallet.on("disconnect", () => console.log("Wallet Disconnected"))
+		}
+		return () => {
+			if(isWalletConnected(WalletState)){
+				WalletState.wallet.wallet.on("accountsChanged", accounts => console.log({accounts}))
+				WalletState.wallet.wallet.on("chainChanged", chainId => console.log({chainId}))
+				WalletState.wallet.wallet.on("disconnect", () => console.log("Wallet Disconnected"))
+			}
+		}
+	}, [WalletState])
 
 	return null
 }
