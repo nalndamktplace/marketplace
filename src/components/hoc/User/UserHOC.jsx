b7bb9bbfe6b6d20f571a61045168bedf2105f92e
z3t4z00k <zetazook@gmail.com>
2022-07-13 18:13:07 +0530

    Added login API call

--- src/components/hoc/User/UserHOC.jsx
+++ src/components/hoc/User/UserHOC.jsx
@@ -1,29 +1,55 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { setUser, unsetUser } from '../../../store/actions/user'
-
-import { useSelector } from 'react-redux'
-import { isUserLoggedIn } from '../../../helpers/functions'
-import { getData, logout } from '../../../helpers/storage'
-import Constants from '../../../config/constants'
+import axios from 'axios'
 import moment from 'moment'
-import { useNavigate } from 'react-router'
+import { useAuth0 } from '@auth0/auth0-react'
+
 import { clearWallet } from '../../../store/actions/wallet'
 import { setSnackbar } from '../../../store/actions/snackbar'
-import { useAuth0 } from '@auth0/auth0-react'
+import { setUser, unsetUser } from '../../../store/actions/user'
+import { hideSpinner, showSpinner } from '../../../store/actions/spinner'
+
+import Constants from '../../../config/constants'
+import { BASE_URL } from '../../../config/env'
+import { getData, logout } from '../../../helpers/storage'
+import { isUsable, isUserLoggedIn } from '../../../helpers/functions'
 
 const UserHOC = props => {
 
-	const { user, isAuthenticated, isLoading } = useAuth0();
+	const Auth0 = useAuth0()
 
 	const dispatch = useDispatch()
 
 	const UserState = useSelector(state => state.UserState)
 
+	const [Loading, setLoading] = useState(false)
+
 	useEffect(() => {
-		console.log({user})
-	}, [user])
+		if(isUsable(Auth0.error)) dispatch(setSnackbar('ERROR'))
+	}, [Auth0.error, dispatch])
+
+	useEffect(() => {
+		if(Loading || Auth0.isLoading) dispatch(showSpinner())
+		else dispatch(hideSpinner())
+	}, [Loading, Auth0.isLoading, dispatch])
+
+	useEffect(() => {
+		if(Auth0.isAuthenticated && isUsable(Auth0.user)){
+			setLoading(true)
+			axios({
+				url: `${BASE_URL}/api/user/oauth/login`,
+				method: 'POST',
+				data: Auth0.user
+			}).then(res => {
+				if(res.status === 200) {
+					dispatch(setUser(res.data))
+				}
+			}).catch(err => {
+				dispatch(setSnackbar('ERROR'))
+			}).finally(() => setLoading(false))
+		}
+	}, [Auth0.isAuthenticated ,Auth0.user, dispatch])
 
 	useEffect(() => {
 		if(!isUserLoggedIn(UserState)){
@@ -31,6 +57,7 @@ const UserHOC = props => {
 			if(isUserLoggedIn(userData)){
 				const accessTokenExpiry = userData.tokens.acsTkn.exp
 				if(moment(accessTokenExpiry).diff(moment())<0){
+					Auth0.logout()
 					dispatch(unsetUser())
 					dispatch(clearWallet())
 					dispatch(setSnackbar({show: true, message: "You have been logged out.", type: 3}))
@@ -38,7 +65,7 @@ const UserHOC = props => {
 				}
 			}
 		}
-	}, [UserState, dispatch])
+	}, [UserState, dispatch, Auth0])
 
 	return null
 }
