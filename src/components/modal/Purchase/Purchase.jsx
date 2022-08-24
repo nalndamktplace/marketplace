367fd571ef06bd6f20d52000f543986185c8fb9f
z3t4z00k <zetazook@gmail.com>
2022-08-24 19:38:04 +0530

    Updated purchase modal interface

--- src/components/modal/Purchase/Purchase.jsx
+++ src/components/modal/Purchase/Purchase.jsx
@@ -13,9 +13,11 @@ import { isFilled, isUsable } from '../../../helpers/functions'
 import { hideSpinner, showSpinner } from '../../../store/actions/spinner'
 import { SHOW_PURCHASE_MODAL } from '../../../store/actions/modal'
 
+import { ReactComponent as CloseIcon } from "../../../assets/icons/close-icon.svg"
+
 import { BASE_URL } from '../../../config/env'
 
-const PurchaseModal = props => {
+const PurchaseModal = ({onOldBookPurchase,onNewBookPurchase,data}) => {
 
 	const dispatch = useDispatch()
 
@@ -25,7 +27,59 @@ const PurchaseModal = props => {
 	const [Show, setShow] = useState(false)
 	const [Offers, setOffers] = useState([])
 	const [Loading, setLoading] = useState(false)
-	const [ActiveTab, setActiveTab] = useState(0)
+	const [ShowOffers, setShowOffers] = useState(0)
+	const [ActiveOffer, setActiveOffer] = useState(null)
+
+	const renderOffers = () => {
+		let offersDOM = []
+		if(isFilled(Offers)){
+			Offers.forEach(offer => {
+				offer.previous_owner === ActiveOffer?.previous_owner?
+					offersDOM.push(
+						<div className="purchase__wrapper__content__offers__item purchase__wrapper__content__offers__item--active" key={offer.book_address+'-'+offer.order_id}>
+							<p className='utils__margin__bottom--n utils__d__flex utils__align__center'>Price:&nbsp;{offer.price===0?"FREE":<><img src='https://imagedelivery.net/yOWneHxM1h9mu46Te3Yjwg/59c27d12-e4eb-4f74-7a6e-b33ba6537600/icon48' style={{width: 20, height: 20, objectFit: 'contain'}} alt="USDC"/>&nbsp;{isUsable(offer.price)?parseFloat(offer.price).toFixed(2):null}</>}</p>
+							<p className='utils__margin__bottom--n'>DA Score: {isUsable(offer.da_score)?parseFloat(offer.da_score).toFixed(2):null}</p>
+							<p className='utils__margin__bottom--n'>Seller: {offer.previous_owner}</p>
+						</div>
+					)
+				:
+					offersDOM.push(
+						<div onClick={()=>setActiveOffer(offer)} className="purchase__wrapper__content__offers__item" key={offer.book_address+'-'+offer.order_id}>
+							<p className='utils__margin__bottom--n utils__d__flex utils__align__center'>Price:&nbsp;{offer.price===0?"FREE":<><img src='https://imagedelivery.net/yOWneHxM1h9mu46Te3Yjwg/59c27d12-e4eb-4f74-7a6e-b33ba6537600/icon48' style={{width: 20, height: 20, objectFit: 'contain'}} alt="USDC"/>&nbsp;{isUsable(offer.price)?parseFloat(offer.price).toFixed(2):null}</>}</p>
+							<p className='utils__margin__bottom--n'>DA Score: {isUsable(offer.da_score)?parseFloat(offer.da_score).toFixed(2):null}</p>
+							<p className='utils__margin__bottom--n'>Seller: {offer.previous_owner}</p>
+						</div>
+					)
+			})
+		}
+		if(offersDOM.length===0) offersDOM.push(<div className="typo__color--n600 utils__d__flex utils__justify__center">No Offers</div>)
+		return offersDOM
+	}
+
+	const getClasses = () => {
+		let classes = ["purchase purchase__wrapper"]
+		if (Show) classes.push("purchase__wrapper--open")
+		else classes.push("purchase__wrapper--close")
+		return classes.join(" ")
+	}
+
+	const getLowestPrice = () => {
+		if(isFilled(Offers)){
+			const offers = Offers.sort((offera, offerb) => {
+				return offera.price > offerb.price
+			})
+			return parseFloat(offers[0].price).toFixed(2)
+		}
+	}
+
+	const onOldPurchaseHandler = () => {
+		if(isUsable(ActiveOffer))onOldBookPurchase(ActiveOffer)
+		else dispatch(setSnackbar({show: true, message: "Please select an old copy.", type: 3}))
+	}
+
+	useEffect(() => {
+		window.document.documentElement.style.overflowY = Show ? "hidden" : "auto"
+	}, [Show])
 
 	useEffect(() => {
 		if(Loading) dispatch(showSpinner())
@@ -41,7 +95,7 @@ const PurchaseModal = props => {
 	}, [ModalState])
 
 	useEffect(() => {
-		if(Show === true && isUsable(props.data)){
+		if(Show === true && isUsable(data)){
 			axios({
 				url: BASE_URL+'/api/book/list',
 				method: 'GET',
@@ -50,7 +104,7 @@ const PurchaseModal = props => {
 					'authorization': `Bearer ${UserState.tokens.acsTkn.tkn}`
 				},
 				params: {
-					bookAddress: props.data.book_address
+					bookAddress: data.book_address
 				}
 			}).then(res => {
 				if(res.status === 200) setOffers(res.data)
@@ -59,93 +113,43 @@ const PurchaseModal = props => {
 				dispatch(setSnackbar('ERROR'))
 			}).finally(() => setLoading(false))
 		}
-	}, [Show, dispatch, props])
-
-	const renderTabs = () => {
-
-		const renderOffers = () => {
-			let offersDOM = []
-			if(isFilled(Offers)){
-				Offers.forEach(offer => {
-					offersDOM.push(
-						<div className="modal__purchase__data__offers__item" key={offer.book_address+'-'+offer.order_id}>
-							<p className='utils__margin__bottom--n'>Order: {offer.order_id}</p>
-							<p className='utils__margin__bottom--n utils__d__flex utils__align__center'>Price: {offer.price===0?"FREE":<><img src='https://imagedelivery.net/yOWneHxM1h9mu46Te3Yjwg/59c27d12-e4eb-4f74-7a6e-b33ba6537600/icon48' style={{width: 20, height: 20, objectFit: 'contain'}} alt="USDC"/>&nbsp;{offer.price}</>}</p>
-							<p className='utils__margin__bottom--n'>Token: {offer.token_id}</p>
-							<p className='utils__margin__bottom--n'>DA Score: {offer.da_score}</p>
-							<p className='utils__margin__bottom--n'>Address: {offer.book_address}</p>
-							<p className='utils__margin__bottom--n'>Seller: {offer.previous_owner}</p>
-							<div className="modal__purchase__data__offers__item__cta">
-								<Button type="primary" onClick={()=>props.onOldBookPurchase(offer)}>Buy Now</Button>
-							</div>
-						</div>
-					)
-				})
-			}
-			if(offersDOM.length===0) offersDOM.push(<div className="typo__color--n600 utils__d__flex utils__justify__center">No Offers</div>)
-			return offersDOM
-		}
-
-		switch (ActiveTab) {
-			case 1:
-				return (
-					<React.Fragment>
-						<div className="modal__purchase__data__offers">
-							{renderOffers()}
-						</div>
-					</React.Fragment>
-				)
-			default:
-				return (
-					<React.Fragment>
-						<div className="modal__purchase__data__book">
-							<div className="modal__purchase__data__book__cover">
-								<img src={props.data.cover_public_url?props.data.cover_public_url:props.data.cover} alt={props.data.title + ' cover'} loading="lazy"/>
-							</div>
-							<div className="modal__purchase__data__book__info">
-								<p className='utils__margin__bottom--n typo__head--6 typo__transform--capital'>{props.data.title}</p>
-								<p className='utils__margin__bottom--n typo__body--3 typo__transform--upper'>{props.data.author}</p>
-								<p className='utils__margin__bottom--n typo__body--2 typo__transform--upper utils__d__flex utils__align__center'>{props.data.price===0?"FREE":<><img src='https://imagedelivery.net/yOWneHxM1h9mu46Te3Yjwg/59c27d12-e4eb-4f74-7a6e-b33ba6537600/icon48' style={{width: 20, height: 20, objectFit: 'contain'}} alt="USDC"/>&nbsp;{props.data.price}</>}</p>
-							</div>
-						</div>
-						<div className="modal__purchase__data__cta">
-							<Button type="primary" onClick={() => props.onNewBookPurchase()}>Buy Now</Button>
-						</div>
-					</React.Fragment>
-				)
-		}
-	}
-	
-	const getTabsClasses = tab => {
-		let classes = ['modal__purchase__tabs__item', 'typo__act typo__act--2']
-		if(tab === ActiveTab) classes.push('modal__purchase__tabs__item--active')
-		else classes.push('utils__cursor--pointer')
-		return classes.join(' ')
-	}
-
-	const switchTab = index => {
-		setActiveTab(index)
-		if(index === 0) GaTracker('tab_view_purchase_modal_new')
-		else GaTracker('tab_view_purchase_modal_old')
-	}
+	}, [Show, dispatch, data, UserState])
 
 	return (
 		<Backdrop show={Show}>
-			<Modal title='Purchase eBook' open={Show} cancellable>
-				<div className="modal__purchase">
-					<div className="modal__purchase__tabs">
-						<div onClick={()=>switchTab(0)} className={getTabsClasses(0)}>
-							New Copy
-						</div>
-						<div onClick={()=>switchTab(1)} className={getTabsClasses(1)}>
-							Old Copy
-						</div>
-					</div>
-					<div className="modal__purchase__data">
-						{renderTabs()}
+			<div className={getClasses()}>
+				<div className="purchase__wrapper__header">
+					<div className="purchase__wrapper__header__title typo__head--6 typo__transform--capital">{data.title.toLowerCase()}</div>
+					<div className="purchase__wrapper__header__close-button">
+						<div onClick={()=>setShow(false)}><CloseIcon width={20} height={20}/></div>
 					</div>
 				</div>
-			</Modal>
+				<div className="purchase__wrapper__content">
+					{ShowOffers?
+						<div className="purchase__wrapper__content__offers">
+							<div className='typo__body typo__body--2 utils__d__flex purchase__wrapper__content__offers__count'>{Offers.length} offer(s) starting from&nbsp;{data.price===0?"FREE":<><img src='https://imagedelivery.net/yOWneHxM1h9mu46Te3Yjwg/59c27d12-e4eb-4f74-7a6e-b33ba6537600/icon48' style={{width: 20, height: 20, objectFit: 'contain'}} alt="USDC"/>&nbsp;{getLowestPrice()}</>}</div>
+							<div className="purchase__wrapper__content__offers__list">
+								{renderOffers()}
+							</div>
+							<div className="purchase__wrapper__content__offers__cta">
+								<Button type='secondary' onClick={()=>setShowOffers(false)} className="purchase__wrapper__content__offers__cta__button">Back</Button>
+								<Button type='primary' onClick={()=>onOldPurchaseHandler()} className="purchase__wrapper__content__offers__cta__button">Purchase</Button>
+							</div>
+						</div>
+					:
+						<div className="purchase__wrapper__content__options">
+							<div className="purchase__wrapper__content__options__item" onClick={()=>onNewBookPurchase()}>
+								<p className='typo__body utils__d__flex typo__align--center'>Purchase new copy for</p>
+								<h5 className='typo__head typo__head--4 typo__transform--upper utils__d__flex utils__align__center'>{data.price===0?"FREE":<><img src='https://imagedelivery.net/yOWneHxM1h9mu46Te3Yjwg/59c27d12-e4eb-4f74-7a6e-b33ba6537600/icon48' style={{width: 32, height: 32, objectFit: 'contain'}} alt="USDC"/>&nbsp;{parseFloat(data.price).toFixed(2)}</>}</h5>
+							</div>
+							<div className="purchase__wrapper__content__options__item" onClick={()=>setShowOffers(true)}>
+								<p className='typo__body utils__d__flex typo__align--center'>Old copies start from</p>
+								<h5 className='typo__head typo__head--4 typo__transform--upper utils__d__flex utils__align__center'>{data.price===0?"FREE":<><img src='https://imagedelivery.net/yOWneHxM1h9mu46Te3Yjwg/59c27d12-e4eb-4f74-7a6e-b33ba6537600/icon48' style={{width: 32, height: 32, objectFit: 'contain'}} alt="USDC"/>&nbsp;{getLowestPrice()}</>}</h5>
+							</div>
+						</div>
+					}
+				</div>
+			</div>
 		</Backdrop>
 	)
 }
