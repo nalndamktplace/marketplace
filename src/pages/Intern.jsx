cd365961a609cae86fbb63ac524d0af9e1d78a2e
z3t4z00k <zetazook@gmail.com>
2022-10-16 23:22:44 +0530

    Improved layout

--- src/pages/Intern.jsx
+++ src/pages/Intern.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const InternHirePage = () => {
 
     return (
-        <iframe title='Form' src="https://docs.google.com/forms/d/e/1FAIpQLSepDk4WMNsRErPMLrp2ujFoG4_gN4gZLQpCAnbHU28Hmcmcqw/viewform?embedded=true" width="640" height="1320" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <iframe title="form" src="https://docs.google.com/forms/d/e/1FAIpQLSepDk4WMNsRErPMLrp2ujFoG4_gN4gZLQpCAnbHU28Hmcmcqw/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     )
 
 }
