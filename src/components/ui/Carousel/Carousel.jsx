d616f3322b13d70d897d899de6c2cfd685b9dd76
Prerna Babber <1941012817.PRERNABABBER@gmail.com>
2022-11-01 15:18:38 +0530

    changed carousel image

--- src/components/ui/Carousel/Carousel.jsx
+++ src/components/ui/Carousel/Carousel.jsx
@@ -33,21 +33,21 @@ function Carousel() {
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img
-              src="https://plus.unsplash.com/premium_photo-1661395010793-dd7735ffe141?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+              src="https://miro.medium.com/max/1600/1*Rv0RecX9Th90htNyv46TAw.png"
               class="d-block w-100"
               alt="..."
             />
           </div>
           <div class="carousel-item">
             <img
-              src="https://plus.unsplash.com/premium_photo-1661395010793-dd7735ffe141?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+              src="https://miro.medium.com/max/1600/1*Rv0RecX9Th90htNyv46TAw.png"
               class="d-block w-100"
               alt="..."
             />
           </div>
           <div class="carousel-item">
             <img
-              src="https://plus.unsplash.com/premium_photo-1661395010793-dd7735ffe141?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+              src="https://miro.medium.com/max/1600/1*Rv0RecX9Th90htNyv46TAw.png"
               class="d-block w-100"
               alt="..."
             />
