48c2271ea5f7d964fef79def2f953b0992a9923b
thevirinchi <zetazook@gmail.com>
2022-11-14 18:41:01 +0530

    Updated config

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,3 @@
-export const BASE_URL = "http://127.0.0.1:8080"
-// export const BASE_URL = "https://some-url.ngrok.io"
+export const BASE_URL = 'http://127.0.0.1:8080'
+// export const BASE_URL = 'https://some-url.ngrok.io'
 // export const BASE_URL = 'https://server.nalnda.com'
-// export const BASE_URL = "https://34.93.60.247:443"
