6572f6e46850c860fa16cbee2bcb5f7d1bdbb3b4
Prerna <1941012817.PRERNABABBER@gmail.com>
2022-12-07 16:44:58 +0530

    added upvote and downvote buttons

--- src/components/ui/Quote/QuoteSection.jsx
+++ src/components/ui/Quote/QuoteSection.jsx
@@ -4,45 +4,44 @@ import moment from 'moment'
 import { useEffect, useState } from "react"
 import { setSnackbar } from "../../../store/actions/snackbar"
 import { BASE_URL } from "../../../config/env"
-import { ReactComponent as BookmarkIcon } from '../../../assets/icons/bookmark.svg'
 import { ReactComponent as CommentIcon } from '../../../assets/icons/comment.svg'
 import { ReactComponent as QuoteIcon } from "../../../assets/icons/quote.svg"
-import { ReactComponent as ShareIcon } from '../../../assets/icons/share.svg'
 import { ReactComponent as UserIcon } from '../../../assets/icons/user.svg'
 import { ReactComponent as LikeIcon } from '../../../assets/icons/thumbs-up.svg'
 import { ReactComponent as DislikeIcon } from '../../../assets/icons/thumbs-down.svg'
 import { ReactComponent as SendIcon } from '../../../assets/icons/send.svg'
+import { ReactComponent as UpVoteIcon } from '../../../assets/icons/arrow-up-circle.svg'
+import { ReactComponent as DownVoteIcon } from '../../../assets/icons/arrow-down-circle.svg'
 import { useDispatch } from 'react-redux'
 
-const QuoteSection = ({ quote, bookMeta, UserState, preview, rendition }) => 
-{
+const QuoteSection = ({ quote, bookMeta, UserState, preview, rendition }) => {
     const dispatch = useDispatch()
 
-    const [CommentSection, setCommentSection] = useState(false) 
+    const [CommentSection, setCommentSection] = useState(false)
     const [postComment, setPostComment] = useState('')
     const [Comments, setComments] = useState([])
 
     useEffect(() => {
-            axios({
-                url: `${BASE_URL}/api/book/quotes/comments`,
-                method: 'GET',
-                params: {
-                    bookAddress: bookMeta.book_address,
-                    quoteId: quote.book_id
-                }
-            }).then(res => {
-                if (res.status === 200) {
-                    setComments(res.data)
-                }
-                else dispatch(setSnackbar('NOT200'))
-            }).catch(err => {
-                dispatch(setSnackbar('ERROR'))
-            })
+        axios({
+            url: `${BASE_URL}/api/book/quotes/comments`,
+            method: 'GET',
+            params: {
+                bookAddress: bookMeta.book_address,
+                quoteId: quote.book_id
+            }
+        }).then(res => {
+            if (res.status === 200) {
+                setComments(res.data)
+            }
+            else dispatch(setSnackbar('NOT200'))
+        }).catch(err => {
+            dispatch(setSnackbar('ERROR'))
+        })
 
-        
+
     }, [bookMeta, dispatch, rendition, preview, quote])
 
-    const postComments = () =>{
+    const postComments = () => {
         if (postComment.length > 2) {
             axios({
                 url: `${BASE_URL}/api/book/quotes/comments`,
@@ -91,18 +90,18 @@ const QuoteSection = ({ quote, bookMeta, UserState, preview, rendition }) =>
         Comments.forEach((comment, i) => {
             commentsDOM.push(
                 <div>
-                <div key={i} className='quotes__comments'>
-                    <div className="quotes__comments__icon">
-                        <UserIcon width={22} height={24} stroke="currentColor" />
-                        <div className='quotes__comments__text'>{comment.uid}</div>
-                    </div>
-                    <div className="quotes__comments__body typo__body">
-                        {comment.comment}
-                    </div>
+                    <div key={i} className='quotes__comments'>
+                        <div className="quotes__comments__icon">
+                            <UserIcon width={22} height={24} stroke="currentColor" />
+                            <div className='quotes__comments__text'>{comment.uid}</div>
+                        </div>
+                        <div className="quotes__comments__body typo__body">
+                            {comment.comment}
+                        </div>
                     </div>
                     <div className='quotes__item__section__icons'>
-                    <div className='quotes__item__section__icon__action'><LikeIcon height={24} width={24} /></div>
-                    <div className='quotes__item__section__icon__action'><DislikeIcon height={24} width={24} /></div>
+                        <div className='quotes__item__section__icon__action'><LikeIcon height={24} width={24} /></div>
+                        <div className='quotes__item__section__icon__action'><DislikeIcon height={24} width={24} /></div>
                     </div>
                     {comment.thread ? renderThreads(comment.thread) : null}
                 </div>
@@ -112,37 +111,37 @@ const QuoteSection = ({ quote, bookMeta, UserState, preview, rendition }) =>
     }
 
 
-  return (
-    <div>
-        <div className='quotes__item'>
-                        <div className="quotes__item__icon">
-                            <QuoteIcon width={32} height={32} stroke="currentColor" />
-                        </div>
-                        <div className="quotes__item__body typo__body">
-                            {quote.body}
-                        </div>
-                        <div className="quotes__item__time typo__cap typo__cap--2">
-                            {moment(quote.created_at).format("D MMM, YYYY") || "-"}
-                        </div>
-                    </div>
-                    <div className="quotes__item__section"></div>
-                    <div className='quotes__item__section__icons'>
-                     
-                        <div className='quotes__item__section__icon__action'> <CommentIcon onClick={()=>setCommentSection(!CommentSection)} height={24} width={24} />{Comments.length}</div>
-                        <div className='quotes__item__section__icon__action'><ShareIcon height={24} width={24} /> Share </div>
-                        <div className='quotes__item__section__icon__action'><BookmarkIcon height={24} width={24} />Save</div>
-                    </div>
-                    {CommentSection?   
-                    <div>
+    return (
+        <div>
+            <div className='quotes__item'>
+                <div className="quotes__item__icon">
+                    <QuoteIcon width={32} height={32} stroke="currentColor" />
+                </div>
+                <div className="quotes__item__body typo__body">
+                    {quote.body}
+                </div>
+                <div className="quotes__item__time typo__cap typo__cap--2">
+                    {moment(quote.created_at).format("D MMM, YYYY") || "-"}
+                </div>
+            </div>
+            <div className="quotes__item__section"></div>
+            <div className='quotes__item__section__icons'>
+
+                <div className='quotes__item__section__icon__action'> <CommentIcon onClick={() => setCommentSection(!CommentSection)} height={24} width={24} />{Comments.length}</div>
+                <div className='quotes__item__section__icon__action'><UpVoteIcon height={24} width={24} /></div>
+                <div className='quotes__item__section__icon__action'><DownVoteIcon height={24} width={24} /></div>
+            </div>
+            {CommentSection ?
+                <div>
                     <div className='quotes__item__section__comment-div'>
-                        <input  value={postComment} onChange={e => setPostComment(e.target.value)} className="quotes__item__section__comment-div__input" type="text" placeholder="Add a comment..." />
-                        <div className='quotes__item__section__comment-div__action'><SendIcon  onClick={() => postComments()} /></div>
+                        <input value={postComment} onChange={e => setPostComment(e.target.value)} className="quotes__item__section__comment-div__input" type="text" placeholder="Add a comment..." />
+                        <div className='quotes__item__section__comment-div__action'><SendIcon onClick={() => postComments()} /></div>
                     </div>
-                        {renderComments()}
-                    </div>: null}
-                  
-    </div>
-  )
+                    {renderComments()}
+                </div> : null}
+
+        </div>
+    )
 }
 
 export default QuoteSection
