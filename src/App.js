1bda1aa8e55c55c107770632fa675205ba75a0bd
thevirinchi <zetazook@gmail.com>
2023-01-10 18:58:41 +0530

    Added missing stylesheet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 
 import { Auth0Provider } from '@auth0/auth0-react'
 
+import '@biconomy/web3-auth/dist/src/style.css'
+
 import './main.scss'
 
 import { Grid } from 'react-spinners-css'
@@ -46,131 +48,67 @@ const RefundPolicyPage = React.lazy(() => import('./pages/Policies/Refund'))
 const ListedBookPage = React.lazy(() => import('./pages/ListedBook'))
 
 const rootReducer = combineReducers({
-    UserState: UserReducer,
-    ModalState: ModalReducer,
-    // WalletState: WalletReducer,
-    SpinnerState: SpinnerReducer,
-    SnackbarState: SnackbarReducer,
-    DarkModeState: DarkModeReducer,
-    BWalletState: BWalletReducer
+	UserState: UserReducer,
+	ModalState: ModalReducer,
+	// WalletState: WalletReducer,
+	SpinnerState: SpinnerReducer,
+	SnackbarState: SnackbarReducer,
+	DarkModeState: DarkModeReducer,
+	BWalletState: BWalletReducer,
 })
 
 const store = createStore(rootReducer)
 
 function App() {
-    let navigate = useNavigate()
-    
-    const onRedirectCallback = (appState) => {
-        navigate(appState?.returnTo || window.location.pathname)
-    }
-    
-    return (
-        <Auth0Provider
-            domain={process.env.REACT_APP_AUTH0_DOMAIN}
-            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-            redirectUri={window.location.origin}
-            onRedirectCallback={onRedirectCallback}
-        >
-            <div className="typo">
-                <Provider store={store}>
-                    <ScrollToTop>
-                        <Suspense
-                            fallback={
-                                <div className={'spinner spinner--show'}>
-                                    <div className="spinner__container">
-                                        <Grid color="#00a2e8" />
-                                    </div>
-                                </div>
-                            }
-                        >
-                            <Routes>
-                                <Route path="/*" element={<IndexPage />} />
-                                <Route path="/" element={<IndexPage />} />
-                                <Route
-                                    path="/hire/intern"
-                                    element={<InternHirePage />}
-                                />
-                                <Route
-                                    path="/book/:bookID"
-                                    element={<BookPage />}
-                                />
-                                <Route
-                                    path="/book/preview"
-                                    element={<ReaderPage />}
-                                />
-                                <Route
-                                    path="/listbook/:bookID"
-                                    element={<ListedBookPage />}
-                                />
-                                <Route
-                                    path="/publish"
-                                    element={
-                                        <ProtectedRoute
-                                            element={<PublishNftPage />}
-                                        />
-                                    }
-                                />
-                                <Route
-                                    path="/publish/ito"
-                                    element={
-                                        <ProtectedRoute
-                                            element={<ItoPublishPage />}
-                                        />
-                                    }
-                                />
-                                <Route
-                                    path="/explore"
-                                    element={<ExplorePage />}
-                                />
-                                <Route
-                                    path="/profile"
-                                    element={
-                                        <ProtectedRoute
-                                            element={<ProfilePage />}
-                                        />
-                                    }
-                                />
-                                <Route
-                                    path="/library"
-                                    element={
-                                        <ProtectedRoute
-                                            element={<LibraryPage />}
-                                        />
-                                    }
-                                />
-                                <Route
-                                    path="/collection"
-                                    element={<CollectionPage />}
-                                />
-                                <Route
-                                    path="/library/reader"
-                                    element={<ReaderPage />}
-                                />
-                                <Route
-                                    path="/policy/terms"
-                                    element={<TermsConditionPage />}
-                                />
-                                <Route
-                                    path="/policy/privacy"
-                                    element={<PrivacyPolicyPage />}
-                                />
-                                   <Route
-                                    path="/policy/refund"
-                                    element={<RefundPolicyPage />}
-                                />
-                                {/* <Route path='/debug/interface' element={<InterfaceDebugPage />}/> */}
-                                {/* <Route path='/debug/wallet' element={<WalletDebugPage/>}/> */}
-                            </Routes>
-                        </Suspense>
-                    </ScrollToTop>
-                    <Snackbar />
-                    <Spinner />
-                    <WalletHOC />
-                    <UserHOC />
-                </Provider>
-            </div>
-        </Auth0Provider>
-    )
+	let navigate = useNavigate()
+
+	const onRedirectCallback = appState => {
+		navigate(appState?.returnTo || window.location.pathname)
+	}
+
+	return (
+		<Auth0Provider domain={process.env.REACT_APP_AUTH0_DOMAIN} clientId={process.env.REACT_APP_AUTH0_CLIENT_ID} redirectUri={window.location.origin} onRedirectCallback={onRedirectCallback}>
+			<div className='typo'>
+				<Provider store={store}>
+					<ScrollToTop>
+						<Suspense
+							fallback={
+								<div className={'spinner spinner--show'}>
+									<div className='spinner__container'>
+										<Grid color='#00a2e8' />
+									</div>
+								</div>
+							}>
+							<Routes>
+								<Route path='/*' element={<IndexPage />} />
+								<Route path='/' element={<IndexPage />} />
+								<Route path='/hire/intern' element={<InternHirePage />} />
+								<Route path='/book/:bookID' element={<BookPage />} />
+								<Route path='/book/preview' element={<ReaderPage />} />
+								<Route path='/listbook/:bookID' element={<ListedBookPage />} />
+								<Route path='/publish' element={<ProtectedRoute element={<PublishNftPage />} />} />
+								<Route path='/publish/ito' element={<ProtectedRoute element={<ItoPublishPage />} />} />
+								<Route path='/explore' element={<ExplorePage />} />
+								<Route path='/profile' element={<ProtectedRoute element={<ProfilePage />} />} />
+								<Route path='/library' element={<ProtectedRoute element={<LibraryPage />} />} />
+								<Route path='/collection' element={<CollectionPage />} />
+								<Route path='/library/reader' element={<ReaderPage />} />
+								<Route path='/policy/terms' element={<TermsConditionPage />} />
+								<Route path='/policy/privacy' element={<PrivacyPolicyPage />} />
+								<Route path='/policy/refund' element={<RefundPolicyPage />} />
+								{/* <Route path='/debug/interface' element={<InterfaceDebugPage />}/> */}
+								{/* <Route path='/debug/wallet' element={<WalletDebugPage/>}/> */}
+							</Routes>
+						</Suspense>
+					</ScrollToTop>
+					<Snackbar />
+					<Spinner />
+					<WalletHOC />
+					<UserHOC />
+				</Provider>
+			</div>
+		</Auth0Provider>
+	)
 }
 
 export default App
