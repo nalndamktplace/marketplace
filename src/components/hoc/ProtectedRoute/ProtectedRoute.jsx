b74900bda81bf3ff94a78cdaa4bdc32c3ce58f51
thevirinchi <zetazook@gmail.com>
2023-01-28 15:59:59 +0530

    Simplified with useWallet hook

--- src/components/hoc/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/hoc/ProtectedRoute/ProtectedRoute.jsx
@@ -1,59 +1,32 @@
 import { useNavigate } from "react-router"
-import { useEffect, useState, useCallback } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
-import Wallet from "../../../connections/wallet"
+import useWallet from "../../../hook/useWallet"
+import { isUserLoggedIn} from "../../../helpers/functions"
 
-import { isUserLoggedIn, isWalletConnected } from "../../../helpers/functions"
-import { setWallet } from "../../../store/actions/wallet"
-import { setSnackbar } from "../../../store/actions/snackbar"
-import { hideSpinner, showSpinner } from "../../../store/actions/spinner"
-import { isUsable } from "../../../helpers/functions"
-
-import { useWeb3AuthContext } from "../../../contexts/SocialLoginContext"
+import { setSnackbar } from '../../../store/actions/snackbar'
+import { showModal, SHOW_SELECT_WALLET_MODEL } from "../../../store/actions/modal"
 
 const ProtectedRoute = ({element}) => {
+	const wallet = useWallet()
 	const dispatch = useDispatch()
 	const navigate = useNavigate()
 
-	const {
-		address,
-		connect,
-		provider,
-	} = useWeb3AuthContext();
-
 	const UserState = useSelector(state => state.UserState)
-	const BWalletState = useSelector(state => state.BWalletState)
 
 	const [isAuthenticated, setIsAuthenticated] = useState(false)
 
-	const loginHandler = async () => {
-		connect();
-	}
-
-	const connectWallet = useCallback(
-		() => {
-			if(!address){
-				loginHandler();
-			}
-			Wallet(provider, dispatch);
-		},[dispatch]
-	)
-
 	useEffect(()=>{
 		if(isUserLoggedIn(UserState)){
-			if(isUsable(BWalletState.smartAccount)){
-				setIsAuthenticated(true)
-			} 
-			else {
-				connectWallet();
-			}
+			if(wallet.isConnected()) setIsAuthenticated(true)
+			else dispatch(showModal(SHOW_SELECT_WALLET_MODEL))
 		}
 		else{
 			dispatch(setSnackbar('NOT_LOGGED_IN'))
 			navigate('/')
 		}
-	},[BWalletState, dispatch, navigate, UserState])
+	},[dispatch, UserState, wallet, navigate])
 
 	return <>{isAuthenticated && element }</>
 }
